729f1f55aaa6ba815db2c51f5edfcc52068a7442
ry00001 <ry00001@protonmail.com>
2018-09-22 23:13:11 +0100

    initial commit, RP base

---
+++ VLCRP/MainLoop.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using DiscordRPC;
+using DiscordRPC.Message;
+using DiscordRPC.RPC;
+
+namespace VLCRP
+{
+    class MainLoop
+    {
+        private bool running;
+        private static DiscordRpcClient client;
+        private RichPresence presence;
+
+        public void Execute()
+        {
+            // todo do things
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CtrlC);
+            running = true;
+            client = new DiscordRpcClient("410474255810035712", null, true, -1); // bind to 1st available pipe
+            client.OnReady += RPCReady;
+
+            Assets assets = new Assets()
+            {
+                LargeImageKey = "cone",
+                LargeImageText = "VLC media player"
+            };
+
+            presence = new RichPresence()
+            {
+                State = "In C#",
+                Details = "Test",
+                Assets = assets
+            };
+
+            client.SetPresence(presence);
+            client.Initialize();
+
+            while (running && client != null)
+            {
+                client.Invoke();
+                Thread.Sleep(15000); // sleep for 15s
+            }
+
+            client.Dispose();
+            Console.WriteLine("Discord RPC shut down.");
+        }
+
+        protected void CtrlC(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("Exiting");
+            client.Dispose();
+            running = false;
+        }
+
+        protected void RPCReady(object sender, ReadyMessage msg)
+        {
+            Console.WriteLine("Ready!");
+        }
+    }
+}
---
+++ VLCRP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLCRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] splash =
+            {
+                "**** VLC RICH PRESENCE ****",
+                "(C) ry00001 2018"
+            };
+            foreach (String m in splash) {
+                Console.WriteLine(m);
+            }
+            new MainLoop().Execute();
+        }
+    }
+}
